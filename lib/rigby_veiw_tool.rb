61239d0992626d61e8a7f94611ffa0cdb4f1a2b9
Jordan Hudgens <jordan@devcamp.com>
2017-08-08 15:31:26 +0000

    Dev tool initial commit

---
+++ lib/rigby_veiw_tool.rb
@@ -0,0 +1,6 @@
+require "rigby_veiw_tool/version"
+require "rigby_veiw_tool/renderer"
+
+module RigbyVeiwTool
+  # Your code goes here...
+end
---
+++ lib/rigby_veiw_tool/version.rb
@@ -0,0 +1,3 @@
+module RigbyVeiwTool
+  VERSION = "0.1.0"
+end
